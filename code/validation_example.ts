5bfedb5e04e6c79253fc27ae2edf62aa7624a9c1
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-12-01 16:15:00 -0300

    validation example created

---
+++ code/validation_example.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+const CustomerSchema = z
+  .object({
+    isCompany: z.boolean(),
+    cpf: z.string().optional(),
+    cnpj: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.isCompany) {
+        return !!data.cnpj;
+      } else {
+        return !!data.cpf;
+      }
+    },
+    {
+      message: "CPF or CNPJ are required depending if its a company or not.",
+      path: ["cpf"],
+    }
+  );
+
+type Customer = z.infer<typeof CustomerSchema>;
+
+const result = CustomerSchema.safeParse({
+  isCompany: true,
+  cpf: "123",
+});
+
+if (!result.success) {
+  console.error(result);
+  console.error(result.error.format());
+} else {
+  const customer: Customer = result.data;
+  console.info(customer);
+}
