90424b384581a1ac323dc341f50ff91227993d54
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-12-02 11:40:17 -0300

    simple usage example updated, .describe() usage added

--- code/simpleUsage.ts
+++ code/simpleUsage.ts
@@ -1,14 +1,17 @@
 import { z } from "zod";
 
 const UserSchema = z.object({
-  name: z.string().min(3, "Name is too short."),
+  name: z.string().min(3, "Name is too short.").describe("User's full name."),
   age: z
     .number()
     .int("Age must be a integer.")
     .positive("Age should be a positive number.")
     .gt(18, "User should be older than 18 years old.")
-    .optional(),
-  email: z.email("E-mail is invalid."),
+    .optional()
+    .describe("User's age."),
+  email: z
+    .email("E-mail is invalid.")
+    .describe("User's valid email address for later contact."),
 });
 
 type User = z.infer<typeof UserSchema>;
